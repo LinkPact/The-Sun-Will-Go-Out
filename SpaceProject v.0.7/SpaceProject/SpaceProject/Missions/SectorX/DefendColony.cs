4a45163fc94a85eebc5c6c230adaa13cc3cf796a
Johan37 <johan.philipsson@hotmail.com>
2014-07-13 14:57:07 +0200

    Remade DefendColony uppdraget.

--- SpaceProject v.0.7/SpaceProject/SpaceProject/Missions/SectorX/DefendColony.cs
+++ SpaceProject v.0.7/SpaceProject/SpaceProject/Missions/SectorX/DefendColony.cs
@@ -23,9 +23,31 @@ namespace SpaceProject
         {
             base.Initialize();
 
-            objectives.Add(new ShootingLevelObjective(Game, this, ObjectiveDescriptions[0],
-                Game.stateManager.overworldState.GetPlanet("New Norrland"), "DefendColony", LevelStartCondition.OnStartMission,
-                new EventTextCapsule(GetEvent((int)EventID.LevelCleared), null, EventTextCanvas.BaseState)));
+            objectives.Add(new ArriveAtLocationObjective(Game, this, ObjectiveDescriptions[0],
+                Game.stateManager.overworldState.GetPlanet("New Norrland"),
+                new EventTextCapsule(GetEvent(0), null, EventTextCanvas.BaseState)));
+
+            objectives.Add(new ShootingLevelObjective(Game, this, ObjectiveDescriptions[1],
+                Game.stateManager.overworldState.GetPlanet("New Norrland"), "DefendColony", LevelStartCondition.TextCleared,
+                new EventTextCapsule(GetEvent(1), null, EventTextCanvas.BaseState)));
+
+            objectives.Add(new ArriveAtLocationObjective(Game, this, ObjectiveDescriptions[2],
+                Game.stateManager.overworldState.GetStation("Fotrun Station I")));
+
+            objectives.Add(new ResponseObjective(Game, this, ObjectiveDescriptions[3],
+                Game.stateManager.overworldState.GetStation("Fotrun Station I"),
+                new ResponseTextCapsule(GetEvent(2), GetAllResponses(2),
+                    new List<System.Action> 
+                                {
+                                    delegate 
+                                    {
+                                        missionHelper.ShowEvent(GetEvent(3));
+                                    },
+                                    delegate 
+                                    {
+                                        missionHelper.ShowEvent(GetEvent(4));
+                                    }
+                                })));
         }
 
         public override void StartMission()
