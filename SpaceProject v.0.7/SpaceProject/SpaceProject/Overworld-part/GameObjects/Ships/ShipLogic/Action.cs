d88b77c0a90abf7e2bcb9beb221fed03e1beff9c
Johan37 <johan.philipsson@hotmail.com>
2014-09-09 21:28:51 +0200

    Added actions to ship AI

---
+++ SpaceProject v.0.7/SpaceProject/SpaceProject/Overworld-part/GameObjects/Ships/ShipLogic/Action.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace SpaceProject
+{
+    public class ShipAction
+    {
+        public virtual void Update(GameTime gameTime) { }
+        public bool Finished;
+    }
+
+    public class CompositeAction : ShipAction
+    {
+        public void Add(ShipAction a)
+        {
+            Actions.Add(a);
+        }
+        protected List<ShipAction> Actions;
+    }
+
+    public class ParallelAction : CompositeAction
+    {
+        public override void Update(GameTime gameTime)
+        {
+            foreach (ShipAction a in Actions)
+            {
+                a.Update(gameTime);
+            }
+            Actions.RemoveAll(a => a.Finished);
+            Finished = Actions.Count == 0;
+        }
+    }
+
+    public class SequentialAction : CompositeAction
+    {
+        public override void Update(GameTime gameTime)
+        {
+            if (Actions.Count > 0)
+            {
+                Actions[0].Update(gameTime);
+                if (Actions[0].Finished)
+                    Actions.RemoveAt(0);
+            }
+            Finished = Actions.Count == 0;
+        }
+    }
+
+    public class PriorityAction : CompositeAction
+    {
+        public override void Update(GameTime gameTime)
+        {
+            foreach (ShipAction a in Actions)
+            {
+                a.Update(gameTime);
+                if (a.Finished)
+                    break;
+            }
+        }
+    }
+}
