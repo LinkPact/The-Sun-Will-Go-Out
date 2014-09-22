d85d149e602434dc1876af161ccbe9b0da413859
Johan37 <johan.philipsson@hotmail.com>
2014-09-22 18:18:16 +0200

    Added more Actions to shipsystem
    OrbitAction and WaitAction

---
+++ SpaceProject v.0.7/SpaceProject/SpaceProject/Overworld-part/GameObjects/Ships/ShipLogic/WaitAction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace SpaceProject
+{
+    class WaitAction : ShipAction
+    {
+        
+        public WaitAction(OverworldShip ship)
+        {
+
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+
+        }
+    }
+}
