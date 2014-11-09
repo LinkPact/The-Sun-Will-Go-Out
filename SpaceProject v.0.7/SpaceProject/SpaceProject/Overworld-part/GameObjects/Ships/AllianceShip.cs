5bb3473aefe289b8d1bf88dd5e36ce98c57333f2
Johan37 <johan.philipsson@hotmail.com>
2014-11-09 20:42:54 +0100

    Made some changes to the Alliance Overworldship

--- SpaceProject v.0.7/SpaceProject/SpaceProject/Overworld-part/GameObjects/Ships/AllianceShip.cs
+++ SpaceProject v.0.7/SpaceProject/SpaceProject/Overworld-part/GameObjects/Ships/AllianceShip.cs
@@ -7,7 +7,7 @@ using Microsoft.Xna.Framework;
 namespace SpaceProject
 {
     /// <summary>
-    /// 
+    /// The alliance smaller ship in the overworld.
     /// </summary>
     public class AllianceShip : OverworldShip
     {
@@ -17,19 +17,18 @@ namespace SpaceProject
 
         public override void Initialize()
         {
-            Class = "RebelShip";
-            name = "Rebel Ship";
+            name = "Alliance Ship";
 
-            sprite = spriteSheet.GetSubSprite(new Rectangle(435, 80, 25, 26));
-            viewRadius = 700;
-            position = new Vector2(0, 0);
-            speed = 0.4f;
-            target = Game.player;
+            sprite = spriteSheet.GetSubSprite(new Rectangle(217, 315, 38, 53));
+            viewRadius = 3000;
+            position = new Vector2(0,0);
+            speed = 0.35f;
 
             centerPoint = new Vector2(sprite.SourceRectangle.Value.Width / 2, sprite.SourceRectangle.Value.Height / 2);
             color = Color.White;
             scale = 1.0f;
             layerDepth = 0.6f;
+            SetDefaultBehavior();
 
             base.Initialize();
         }
@@ -43,23 +42,30 @@ namespace SpaceProject
             destination = new Vector2(position.X + 100, position.Y + 100);
         }
 
+        public void SetDefaultBehavior()
+        {
+            PriorityAction tmpAction = new PriorityAction();
+            tmpAction.Add(new FollowInViewAction(this, Game.player));
+            tmpAction.Add(new PatrolAction(this, Game.stateManager.overworldState.GetSectorX));
+            AIManager = tmpAction;
+            collisionEvent = new PirateColllisionEvent(Game, this, Game.player);
+        }
+
         public void SetPositionInSector()
         {
             Random r = new Random(DateTime.Now.Millisecond);
             position = new Vector2(
                 (int)r.Next(sector.SpaceRegionArea.Left, sector.SpaceRegionArea.Right),
                 (int)r.Next(sector.SpaceRegionArea.Top, sector.SpaceRegionArea.Bottom));
-            //if (CollisionDetection.IsPointInsideRectangle(position, Game.stateManager.overWorldState.HUD.radar.View))
-            //    SetPositionInSector();
         }
+        
         public override void FinalGoodbye()
         {
             IsDead = true;
         }
 
         public override void Update(GameTime gameTime)
-        {
-            // Rotate ship
+        {          
             angle = (float)(MathFunctions.RadiansFromDir(new Vector2(
                 Direction.GetDirectionAsVector().X, Direction.GetDirectionAsVector().Y)) + (Math.PI) / 2 + Math.PI);
 
@@ -68,9 +74,7 @@ namespace SpaceProject
 
         public override void Draw(Microsoft.Xna.Framework.Graphics.SpriteBatch spriteBatch)
         {
-            if (IsUsed)
-                base.Draw(spriteBatch);
+               base.Draw(spriteBatch);
         }
-
     }
 }
