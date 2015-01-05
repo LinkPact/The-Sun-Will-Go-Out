8afa83d7c0a8e03a8872fd4f12d71325c7e01d5f
TehDaniel37 <daniel.alm.grundstrom@live.se>
2015-01-05 21:36:47 +0100

    Cleans up Burn Out-ending code

---
+++ SpaceProject v.0.7/SpaceProject/SpaceProject/MissionStuff/Éndings/Ending.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SpaceProject
+{
+    class Ending
+    {
+        protected Game1 game;
+        protected Sprite spriteSheet;
+
+        protected bool activated;
+        protected bool finished;
+        public bool Finished { get { return finished; } private set { ;} }
+
+        public Ending(Game1 game, Sprite spriteSheet)
+        {
+            this.game = game;
+            this.spriteSheet = spriteSheet;
+        }
+
+        public virtual void Initialize()
+        {
+            
+        }
+
+        public void Activate()
+        {
+            activated = true;
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+            
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            
+        }
+    }
+}
