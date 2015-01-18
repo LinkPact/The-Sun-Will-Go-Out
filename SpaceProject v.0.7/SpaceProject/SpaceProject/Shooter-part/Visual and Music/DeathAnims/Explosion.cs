ab90beab3b192f5288d938d6dea66614bca9b7a9
Jakob37 <jakob.willforss@hotmail.com>
2015-01-18 17:51:44 +0100

    Rest-commit (missed by slow GitHub..)

--- SpaceProject v.0.7/SpaceProject/SpaceProject/Shooter-part/Visual and Music/DeathAnims/Explosion.cs
+++ SpaceProject v.0.7/SpaceProject/SpaceProject/Shooter-part/Visual and Music/DeathAnims/Explosion.cs
@@ -34,6 +34,37 @@ namespace SpaceProject
             this.particleArray = particleArray;
         }
 
+        public void GenerateExplosionParticles(Game1 game, Sprite spriteSheet, GameObjectVertical source, int nbrPartices, float size,
+            float speedFactor = 1, float fragmentDurFactor = 1, float dirFactor = 1, bool randomDir = false)
+        {
+            Vector2 position = source.Position;
+            Vector2 direction = source.Direction * dirFactor;
+            float speed = source.Speed * speedFactor;
+
+            ExplosionParticle[] tempParticleArray = new ExplosionParticle[nbrPartices];
+
+            for (int i = 0; i < nbrPartices; i++)
+                tempParticleArray[i] = new ExplosionParticle(game, spriteSheet, position,
+                                                             direction, speed, size,
+                                                             randomDir, i);
+            particleArray = tempParticleArray;
+        }
+
+        public void GenerateAbsoluteExplosion(Game1 game, Sprite spriteSheet, GameObjectVertical source, int nbrPartices, float size,
+            float speed = 1, int fragmentDur = 1, float fragmentSpeed = 1, bool randomDir = false)
+        {
+            Vector2 position = source.Position;
+            Vector2 direction = source.Direction;
+
+            ExplosionParticle[] tempParticleArray = new ExplosionParticle[nbrPartices];
+
+            for (int i = 0; i < nbrPartices; i++)
+                tempParticleArray[i] = new ExplosionParticle(game, spriteSheet, position,
+                                                             direction, speed, size,
+                                                             randomDir, i, fragmentSpeed, fragmentDur);
+            particleArray = tempParticleArray;
+        }
+
         public Boolean ExplosionFinished()
         {
             foreach (ExplosionParticle par in particleArray)
