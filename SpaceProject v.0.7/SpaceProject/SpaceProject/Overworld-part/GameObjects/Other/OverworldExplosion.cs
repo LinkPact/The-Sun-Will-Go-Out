5f4d93628402c2805cbef7230baff7f43c7c93fb
Johan37 <johan.philipsson@hotmail.com>
2015-02-01 10:36:30 +0100

    Begining of overworld exposions

---
+++ SpaceProject v.0.7/SpaceProject/SpaceProject/Overworld-part/GameObjects/Other/OverworldExplosion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace SpaceProject
+{
+    class OverworldExplosion : GameObjectOverworld
+    {
+        public OverworldExplosion(Game1 Game, Sprite spriteSheet) :
+            base(Game, spriteSheet)
+        {
+
+        }
+    }
+}
