6c6a4ff145913fb579e9e891a70a5d43603fb371
Jakob Willforss <jakob.willforss@hotmail.com>
2015-05-03 14:16:46 +0200

    Starting to set up TheRebelOutpost-mission based on the Astro Dodger mission.

---
+++ SpaceProject v.0.7/SpaceProject/SpaceProject/Missions/Side Missions/Side_TheRebelOutpost.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace SpaceProject
+{
+    public class Side_TheRebelOutpost: Mission
+    {
+        private enum EventID
+        {
+            ShipFound = 0,
+            Return = 1
+        }
+
+        private RebelOutpostAsteroid rebelOutpost;
+        private Sprite overworldSpriteSheet;
+
+        public Side_TheRebelOutpost(Game1 Game, string section, Sprite spriteSheet, MissionID missionID) :
+            base(Game, section, spriteSheet, missionID)
+        {
+            overworldSpriteSheet = new Sprite(Game.Content.Load<Texture2D>("Overworld-Sprites/SectorXSpriteSheet"), null);
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            rebelOutpost = new RebelOutpostAsteroid(Game, overworldSpriteSheet);
+            rebelOutpost.Initialize();
+
+            SetDestinations();
+            SetupObjectives();
+        }
+
+        public override void StartMission()
+        {
+            progress = 0;
+            ObjectiveIndex = 0;
+
+            rebelOutpost.IsUsed = true;
+            Game.stateManager.overworldState.AddOverworldObject(rebelOutpost);
+        }
+
+        public override void OnLoad()
+        {
+            if (!Game.stateManager.overworldState.ContainsOverworldObject(rebelOutpost) &&
+                (MissionState == StateOfMission.Active || MissionState == StateOfMission.Completed ||
+                MissionState == StateOfMission.Failed))
+            {
+                rebelOutpost.IsUsed = true;
+                Game.stateManager.overworldState.AddOverworldObject(rebelOutpost);
+            }
+        }
+
+        public override void MissionLogic()
+        {
+            base.MissionLogic();
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+        }
+
+        public override int GetProgress()
+        {
+            return progress;
+        }
+
+        public override void SetProgress(int progress)
+        {
+            this.progress = progress;
+        }
+
+        protected override void SetDestinations()
+        {
+            destinations = new List<GameObjectOverworld>();
+
+            GameObjectOverworld borderStation =
+                Game.stateManager.overworldState.GetStation("Border Station");
+
+            AddDestination(rebelOutpost, 2);
+            AddDestination(borderStation);
+        }
+
+        protected override void SetupObjectives()
+        {
+            objectives.Clear();
+
+            objectives.Add(new ArriveAtLocationObjective(
+                Game,
+                this,
+                ObjectiveDescriptions[0],
+                new EventTextCapsule(
+                    GetEvent((int)EventID.ShipFound),
+                    null, EventTextCanvas.MessageBox)));
+
+            objectives.Add(new ShootingLevelObjective(
+                Game,
+                this,
+                ObjectiveDescriptions[0],
+                "AstroDodger",
+                LevelStartCondition.Immediately,
+                new EventTextCapsule(
+                    GetEvent((int)EventID.Return),
+                    new EventText("You decide it's best to abandon the ship and return to Fortrun Station. No reward is worth getting crushed by asteroids."),
+                    EventTextCanvas.MessageBox)));
+
+            objectives.Add(new ArriveAtLocationObjective(
+                Game,
+                this,
+                ObjectiveDescriptions[1]));
+        }
+    }
+}
---
+++ SpaceProject v.0.7/SpaceProject/SpaceProject/Overworld-part/GameObjects/Sub Interactive Objects/Instances/RebelOutpostAsteroid.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SpaceProject
+{
+    public class RebelOutpostAsteroid : SubInteractiveObject
+    {
+        public RebelOutpostAsteroid(Game1 Game, Sprite spriteSheet) :
+            base(Game, spriteSheet)
+        {
+
+        }
+
+        public override void Initialize()
+        {
+            sprite = spriteSheet.GetSubSprite(new Rectangle(724, 1075, 54, 55));
+            
+            
+            position = MathFunctions.CoordinateToPosition(new Vector2(1917, 135));
+            name = "RebelOutpostAsteroid";
+
+            base.Initialize();
+
+            SetupText("The Rebel Outpost Asteroid!");
+
+            //SetupText("I think Jakob is experimenting with Space Objects!");
+            //String encounterText = "Rebels! Let's root em out!";
+            //String winText = "We scared them of, for now..";
+            //String loseText = "They are too strong. Let's come back later.";
+            //List<Item> rewards = new List<Item>();
+            //rewards.Add(new BasicLaserWeapon(Game, ItemVariety.regular));
+            //SetupLevel(encounterText, "sub1", 100, rewards, winText, loseText);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+        }
+
+        public override void Interact()
+        {
+            base.Interact();
+        }
+
+        protected override void SetClearedText()
+        {
+            clearedText = "Not applicable?";
+        }
+    }
+}
